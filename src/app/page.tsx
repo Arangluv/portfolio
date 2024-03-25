12c9dedf34b4c984648b0a033f3a96a70c75066a
togethercodingArang <ruhunsu3@naver.com>
2024-03-25 16:14:45 +0900

    공부를 위해 임시로 사용한 style 파일 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import * as styles from "./styles.css";
 // import { myBtn } from "./styles.css";
 
 export default function Home() {
   return (
     <main>
-      <button className={styles.myBtn}>here is button</button>
+      <button>here is button</button>
     </main>
   );
 }
--- src/app/theme2.css.ts
+++
@@ -1,9 +0,0 @@
-// .src/theme2.css.ts
-
-import { createTheme } from "@vanilla-extract/css";
-
-export const [themeClass, vars] = createTheme({
-  color: {
-    fontColor: "blue",
-  },
-});
